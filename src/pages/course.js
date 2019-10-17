45822224dbdf536eecb5e4283dc200426d0a39aa
soyjavi <hello@soyjavi.com>
2019-10-17 20:43:14 +0700

    new style

--- src/pages/course.js
+++ src/pages/course.js
@@ -1,11 +1,18 @@
 import { render } from '../common';
 
+const IMAGE = '/static/course-blockchain-main.gif';
+
 export default (req, res) => {
   res.send(render('index', {
     page: 'course',
     context: 'CURSOS',
+    title: 'Curso Blockchain',
+    description: 'Construye desde cero una Blockchain y Criptomoneda. ¡Descubre las ideas de ingeniería detrás de tecnologías como Bitcoin y Ethereum!',
+    image: IMAGE,
     content: render('course', {
+      image: IMAGE,
       bannerSubscribe: render('templates/bannerSubscribe', { type: 'course' }),
+      dialog: render('templates/dialogCheckout'),
       footer: render('templates/footer'),
     }),
   }));
