2bfd129a424e4f9bfcdd1b565003b767a82acbc8
soyjavi <hello@soyjavi.com>
2019-10-19 09:50:49 +0700

    New error handler

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -5,7 +5,14 @@ export default (error, req, res, next) => {
   else {
     const { code = 400 } = error;
     const html = render('index', {
-      main: JSON.stringify({ code, error: error.message }),
+      page: 'error',
+      context: 'error',
+      content: render('error', {
+        code,
+        message: error.message,
+        bannerSubscribe: render('templates/bannerSubscribe', { type: 'post' }),
+
+      }),
     });
 
     res.status(code).send(html);
